c22442f4339994fc8f11c2b8150e91287acb7d10
Marcin Węgrowski <marcinwg97@gmail.com>
2021-01-16 16:44:33 +0100

    wystylowanie wyników testu

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -4,13 +4,43 @@
 <div class="container px-0" style="background-color: white">
     <div class="row justify-content-center">
         <div class="col-12">
-            <div class="row mx-0">
-                <p class="col-12 text-left pt-3">Strona ładowała się <b>{!! isset($time) ? $time : "" !!} sekundy</b>, czas odpowiedzi wynosił <b>{!! isset($request) ? $request : "" !!} sekundy</b>,
-                rozmiar strony to <b>{!! isset($size) ? $size : "" !!} bajtów</b>.<br>
-                Strona załadowała się szybciej niż <b>{!! isset($percentage) ? round($percentage) : "" !!}%</b> stron w naszym rankingu.</p>
+        <h1 class="lead m-4">Wyniki dla strony {!! isset($page) ? $page : "" !!}</h1>
+            <div class="row mx-0 mt-4">
+                <div class="col-lg-3 col-12">
+                    <div class="status-block">
+                        <div class="status-block-box">
+                            <div class="block-label">Czas ładowania</div>
+                            <div class="block-field"><span class="big">{!! isset($time) ? $time : "" !!}</span><span> s</span></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-12">
+                    <div class="status-block">
+                        <div class="status-block-box">
+                            <div class="block-label">Rozmiar strony</div>
+                            <div class="block-field"><span class="big">{!! isset($size) ? $size : "" !!}</span><span> B</span></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-12">
+                    <div class="status-block">
+                        <div class="status-block-box">
+                            <div class="block-label">czas odpowiedzi</div>
+                            <div class="block-field"><span class="big">{!! isset($request) ? $request : "" !!}</span><span> s</span></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-12">
+                    <div class="status-block">
+                        <div class="status-block-box">
+                            <div class="block-label">Ładuje się szybciej niż</div>
+                            <div class="block-field"><span class="big">{!! isset($percentage) ? round($percentage) : "" !!}</span><span> %</span></div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="px-3" style="width: 100%;margin: 0 auto;">
+        <div class="px-3" style="width: 50%;margin: 0 auto;">
             {!! $chartAvg->container() !!}
         </div>
         {!! $chartAvg->script() !!}
@@ -19,7 +49,7 @@
                 <thead>
                     <th>#</th>
                     <th class="text-left">Strona</th>
-                    <th>Czas ładowania w ms</th>
+                    <th>Czas ładowania (s)</th>
                 </thead>
                 <tbody>
                     @foreach ($results_ranking as $page)
